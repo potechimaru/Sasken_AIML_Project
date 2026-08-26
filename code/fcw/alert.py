d4a8063f48a2ae3a68aa36f439e902b9e6f7028f
420omz <zg07v0guvqua7ts0sidb@docomo.ne.jp>
2026-08-26 12:01:13 +0530

    add 3counter function

--- code/fcw/alert.py
+++ code/fcw/alert.py
@@ -13,7 +13,7 @@ ON/OFFすることを防ぐ。状態はExperiment 3のtrack_idごとに保持す
 class AlertDecision:
     """車両ごとのTTC警報状態をヒステリシス付きで管理する。"""
 
-    def __init__(self, on_threshold=5.0, off_threshold=8.5):
+    def __init__(self, on_threshold=5.0, off_threshold=8.5, required_count=3):
         if on_threshold >= off_threshold:
             raise ValueError(
                 "on_threshold must be smaller than off_threshold"
@@ -21,23 +21,38 @@ class AlertDecision:
 
         self.on_threshold = on_threshold
         self.off_threshold = off_threshold
+        self.required_count = required_count
         self.alert_status = {}
+        self.counter = {}
+
 
     def update(self, ttc, track_id):
         """TTCから指定車両の警報状態を更新して返す。"""
         alert = self.alert_status.setdefault(track_id, False)
+        count = self.counter.setdefault(track_id, 0)
 
-        # 履歴不足または非接近では、接近警報を解除する。
-        if ttc is None:
-            alert = False
-        elif not alert and ttc < self.on_threshold:
-            alert = True
-        elif alert and ttc > self.off_threshold:
-            alert = False
+        #alertがON
+        if alert is True:
+            if ttc is not None and ttc >= self.off_threshold:
+                alert = False
+                count = 0
+            else:
+                alert = True
+        #alertがOFF
+        else:
+            if ttc is not None and ttc <= self.on_threshold:
+                count += 1
+                if count >= self.required_count:
+                    alert = True
+                else:
+                    alert = False
 
         self.alert_status[track_id] = alert
+        self.counter[track_id] = count
+
         return alert
 
     def drop(self, track_id):
         """追跡が終了した車両の警報状態を破棄する。"""
         self.alert_status.pop(track_id, None)
+        self.counter.pop(track_id, None)
