b80180584ecc8f9742a4be24d347819d94768263
KentoMimura <167739762+xpenguin002x@users.noreply.github.com>
2026-09-02 17:11:49 +0530

    roi完成

--- codeC/avp_fcw_roi.h
+++ codeC/avp_fcw_roi.h
@@ -1,29 +1,23 @@
-#ifndef FCW_ROI_H
-#define FCW_ROI_H
+#ifndef FCW_ROI_H_
+#define FCW_ROI_H_
 
-typedef struct
-{
-    vx_f;loat32 x;
-    vx_float32 y
-}fcw_roi_point_t;
-
-typedef struct
-{
-fcw_roi_point_t top_left;
-fcw_roi_point_t top_right;
-fcw_roi_point_t bottom_left;
-fcw_roi_point_t bottom_right;
-}fcw_roi_t;
+#include <VX/vx.h>
+#include "avp_fcw_types.h"
 
 
-void fcw_roi_init(
-    vx_float32 width,
-    vx_int32 height,
-    fcw_roi_t *roi
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+/* 車両BBoxの下辺中央がROI内か判定する */
+vx_bool fcw_roi_check_car(
+    const FcwCar *car
 );
 
 
-
-
-
+#ifdef __cplusplus
+}
 #endif
+
+#endif /* FCW_ROI_H_ */
